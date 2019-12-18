08dfa5ecde6074c3be0f4ab04918587fc11c1704
Amit Supugade <asupugad@redhat.com>
2019-12-18 11:39:16 -0500

    Updated to add date and sheetlink

--- VSPerf.py
+++ VSPerf.py
@@ -7,6 +7,7 @@ import argparse
 import json
 import os
 import sys
+from datetime import date
 from GoogleSheet import GoogleSheet
 from GoogleDrive import GoogleDrive
 
@@ -18,6 +19,9 @@ class VSPerfReport(object):
         self.titles = ['Perf']
         self.gsheet = GoogleSheet(self.template, self.titles, "Nightly Test Report")
 
+        self.date = date.today().isoformat()
+        self.sheet_link = 'https://docs.google.com/spreadsheets/d/' + self.template
+
         self.data_64 = []
         self.data_1500 = []
         self.update_current()
@@ -25,18 +29,21 @@ class VSPerfReport(object):
         self.update_data_to_report()
 
     def update_current(self):
-        current_data_64 = self.gsheet.get_batch_data(self.template, "Perf!D3:AA5")
-        self.gsheet.update_batch_data_row(self.template, "Perf!C3:Z5", current_data_64["valueRanges"][0]["values"])
-        current_data_1500 = self.gsheet.get_batch_data(self.template, "Perf!D8:AA10")
-        self.gsheet.update_batch_data_row(self.template, "Perf!C8:Z10", current_data_1500["valueRanges"][0]["values"])
+        current_data_64 = self.gsheet.get_batch_data(self.template, "Perf!C1:P5")
+        self.gsheet.update_batch_data_row(self.template, "Perf!D1:Q5", current_data_64["valueRanges"][0]["values"])
+        current_data_1500 = self.gsheet.get_batch_data(self.template, "Perf!C8:P12")
+        self.gsheet.update_batch_data_row(self.template, "Perf!D8:Q12", current_data_1500["valueRanges"][0]["values"])
 
     def get_from_testsheet(self):
         self.data_64 = self.gsheet.get_row_data(self.testsheet, "Results!B2:D2")
         self.data_1500 = self.gsheet.get_row_data(self.testsheet, "Results!B24:D24")
 
     def update_data_to_report(self):
-        self.gsheet.update_batch_data_col(self.template, "Perf!AA3:AA5", self.data_64)
-        self.gsheet.update_batch_data_col(self.template, "Perf!AA8:AA10", self.data_1500)
+        self.gsheet.update_columns(self.template, self.date , "Perf!C1")
+        self.gsheet.update_batch_data_col(self.template, "Perf!C3:C5", self.data_64)
+        self.gsheet.update_batch_data_col(self.template, "Perf!C8:C10", self.data_1500)
+        self.gsheet.update_columns(self.template, self.sheet_link, "Perf!C12")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='OVS OFFLOAD Results to Google sheets')
